478548369b1609985812ccc4dd2e7d0d1d7cc8f8
Cyruschoi888 <cyruc.design@gmail.com>
2020-02-13 21:12:47 -0800

    week2 hw change

---
+++ cyrus.choi/m01/index.php
@@ -0,0 +1,296 @@
+
+ <!DOCTYPE html>
+<html lang="en">
+<head>
+  <?php include "parts/head.php"?>
+</head>
+<body>
+<?php include "parts/header-scroll.php"?>
+
+<?php include "parts/header-home.php"?>
+
+  <!-- Hero Homepage ----------------------------------------------------------------------------------------->
+
+  <section class="heroslice bc-dark-2">
+  <div>
+
+      <div class="container-hero ac al-s hero-text-animation grid">
+      <div class="row gutters">
+
+      <div class="col-sm-12">
+        <div class="text-vertical-center heroslice text-vertical-center-padding-home">
+        <div class="hero-l fc-light-1 ">I design things <span class="fc-home">people need</span></div>
+        <h2 class="fc-light-3 pt-s">Hi, I’m Cyrus, a Product Designer based in San Francisco, CA.</h2>
+        <div class="pt-l">
+        <a href="#projects"><div class="button-default button-style-home item-center-horizontal">View Portfolio</div></a>
+        </div>
+        </div>
+      </div>
+
+      </div>
+      </div>
+  </div>
+  </section>
+
+
+  <!-- 6 Projects (Large) ---------------------------------------------------------------------------------------->
+
+  <section>
+    <div class="grid" id="projects">
+    <div class="row">
+      <div class="col-sm-12 col-md-6 dis-s">
+      <a href="gs.php">
+          <div class="home_projects">
+          <img src="img/home/gs-l.jpg" alt="Crunchyroll Website" class="home_projects_image">
+          <div class="home_projects_overlay">
+          <div class="home_projects_text">
+            <h3>MOBILE UI DESIGN</h3>
+            <h1 class="fc-light-1 pt-s">Crunchyroll Website</h1>
+            <p class="fc-light-3 pt-s">Website redesign to improve user experience of streaming anime. </p>
+          </div>
+          </div>
+          </div>
+      </a>
+      </div>
+
+      <div class="col-sm-12 col-md-6 dis-s">
+      <a href="bh.php">
+          <div class="home_projects">
+          <img src="img/home/bh-l.jpg" alt="Grocery Shopper" class="home_projects_image">
+          <div class="home_projects_overlay">
+          <div class="home_projects_text">
+            <h3>IOT PRODUCT</h3>
+            <h1 class="fc-light-1 pt-s">Brave Hero</h1>
+            <p class="fc-light-3 pt-s">Builds unaccompanied minor passengers' confidence and increase their safety in flight.</p>
+          </div>
+          </div>
+          </div>
+      </a>
+      </div>
+
+      <div class="col-sm-12 col-md-6 dis-s">
+      <a href="halo.php">
+          <div class="home_projects">
+          <img src="img/home/halo-l.jpg" alt="Grocery Shopper" class="home_projects_image">
+          <div class="home_projects_overlay">
+          <div class="home_projects_text">
+            <h3>AR/UI DESIGN</h3>
+            <h1 class="fc-light-1 pt-s">Halo</h1>
+            <p class="fc-light-3 pt-s">Provides emotional support to pet owners through the end of their beloved pets' journey.</p>
+          </div>
+          </div>
+          </div>
+      </a>
+      </div>
+
+      <div class="col-sm-12 col-md-6 dis-s">
+      <a href="fp.php">
+          <div class="home_projects">
+          <img src="img/home/fp-l.jpg" alt="Grocery Shopper" class="home_projects_image">
+          <div class="home_projects_overlay">
+          <div class="home_projects_text">
+            <h3>VISUAL DESIGN</h3>
+            <h1 class="fc-light-1 pt-s">Floating Parkour</h1>
+            <p class="fc-light-3 pt-s">Helps tracers challenge their parkour skills and overcome different environments.</p>
+          </div>
+          </div>
+          </div>
+      </a>
+      </div>
+
+      <div class="col-sm-12 col-md-6 dis-s">
+      <a href="fc.php">
+          <div class="home_projects">
+          <img src="img/home/fc-l.jpg" alt="Grocery Shopper" class="home_projects_image">
+          <div class="home_projects_overlay">
+          <div class="home_projects_text">
+            <h3>MOBILE DEVELOPMENT</h3>
+            <h1 class="fc-light-1 pt-s">Frenchie Catch</h1>
+            <p class="fc-light-3 pt-s">Tracks Frenchie's Location.</p>
+          </div>
+          </div>
+          </div>
+      </a>
+      </div>
+
+      <div class="col-sm-12 col-md-6 dis-s">
+      <a href="aroma.php">
+          <div class="home_projects">
+          <img src="img/home/aroma-l.jpg" alt="Grocery Shopper" class="home_projects_image">
+          <div class="home_projects_overlay">
+          <div class="home_projects_text">
+            <h3>MOTION GRAPHICS</h3>
+            <h1 class="fc-light-1 pt-s">Aroma</h1>
+            <p class="fc-light-3 pt-s">Futuristic Interface.</p>
+          </div>
+          </div>
+          </div>
+      </a>
+      </div>
+
+    </div>
+    </div>
+  </section>
+
+
+<!-- 6 Projects (Small) ---------------------------------------------------------------------------------------->
+
+  <section class="dis-l">
+  <div class="homepage-small-project-card-frame">
+  <a href="gs.php">
+  <div class="overlay_frame">
+    <img src="img/home/gs-s.jpg" class="image">
+    <div class="overlay_text">
+      <div class="container grid">
+      <div class="row gutters">
+      <div class="col-sm-12">
+        <h3 class="pt fc-middle">MOBILE UI DESIGN</h3>
+        <h1 class="pt-s fc-dark-1">Grocery Shopper</h1>
+        <p class="pt-s fc-dark-3">Supports who is not able to access a local store for their grocery needs.</p>
+      </div>
+      </div>
+      </div>
+    </div>
+  </div>
+  </a>
+  </div>
+  </section>
+
+  <section class="dis-l">
+  <div class="homepage-small-project-card-frame">
+  <a href="bh.php">
+  <div class="overlay_frame">
+    <img src="img/home/bh-s.jpg" class="image">
+    <div class="overlay_text">
+      <div class="container grid">
+      <div class="row gutters">
+      <div class="col-sm-12">
+        <h3 class="pt fc-middle">IOT PRODUCT</h3>
+        <h1 class="pt-s fc-dark-1">Brave Hero</h1>
+        <p class="pt-s fc-dark-3">Builds unaccompanied minor passengers' confidence and increase their safety in flight.</p>
+      </div>
+      </div>
+      </div>
+    </div>
+  </div>
+  </a>
+  </div>
+  </section>
+
+  <section class="dis-l">
+   <div class="homepage-small-project-card-frame">
+  <a href="halo.php">
+  <div class="overlay_frame">
+    <img src="img/home/halo-s.jpg" class="image">
+    <div class="overlay_text">
+      <div class="container grid">
+      <div class="row gutters">
+      <div class="col-sm-12">
+        <h3 class="pt fc-middle">AR/UI DESIGN</h3>
+        <h1 class="pt-s fc-dark-1">Halo</h1>
+        <p class="pt-s fc-dark-3">Provides emotional support to pet owners through the end of their beloved pets' journey.</p>
+      </div>
+      </div>
+      </div>
+    </div>
+  </div>
+  </a>
+  </div>
+  </section>
+
+  <section class="dis-l">
+   <div class="homepage-small-project-card-frame">
+  <a href="fp.php">
+  <div class="overlay_frame">
+    <img src="img/home/fp-s.jpg" class="image">
+    <div class="overlay_text">
+      <div class="container grid">
+      <div class="row gutters">
+      <div class="col-sm-12">
+        <h3 class="pt fc-middle">VISUAL DESIGN</h3>
+        <h1 class="pt-s fc-dark-1">Floating Parkour</h1>
+        <p class="pt-s fc-dark-3">Helps tracers challenge their parkour skills and overcome different environments.</p>
+      </div>
+      </div>
+      </div>
+    </div>
+  </div>
+  </a>
+  </div>
+  </section>
+
+  <section class="dis-l">
+   <div class="homepage-small-project-card-frame">
+  <a href="fc.php">
+  <div class="overlay_frame">
+    <img src="img/home/fc-s.jpg" class="image">
+    <div class="overlay_text">
+      <div class="container grid">
+      <div class="row gutters">
+      <div class="col-sm-12">
+        <h3 class="pt fc-middle">MOBILE DEVELOPMENT</h3>
+        <h1 class="pt-s fc-light-1">Frenchie Catch</h1>
+        <p class="pt-s fc-light-3">Tracks Frenchie's Location.</p>
+      </div>
+      </div>
+      </div>
+    </div>
+  </div>
+  </a>
+  </div>
+  </section>
+
+  <section class="dis-l">
+   <div class="homepage-small-project-card-frame">
+  <a href="aroma.php">
+  <div class="overlay_frame">
+    <img src="img/home/aroma-s.jpg" class="image">
+    <div class="overlay_text">
+      <div class="container grid">
+      <div class="row gutters">
+      <div class="col-sm-12">
+        <h3 class="pt fc-middle">MOTION GRAPHICS</h3>
+        <h1 class="pt-s fc-dark-1">Aroma</h1>
+        <p class="pt-s fc-dark-3">Futuristic Interface.</p>
+      </div>
+      </div>
+      </div>
+    </div>
+  </div>
+  </a>
+  </div>
+  </section>
+
+
+  <!-- About Me ------------------------------------------------------------------------------------->
+
+  <section class="pt pb" id="about">
+  <div class="container grid">
+  <div class="row gutters">
+
+        <div class="col-sm-12 col-md-6">
+          <img src="img/home/profileimg.jpg" alt="About Me" class="image">
+        </div>
+
+        <div class="col-sm-1 dis-s"></div>
+
+        <div class="col-sm-12 col-md-5 text-vertical-center">
+           <div class="hero-s">About Me</div>
+           <p class="pt-s pb-s">I am passionate about developing scenario-based user interaction designs as well as devising a digital or physical product. Approach product design by using different forms and looking at every solution from a user experience point of view always amazes and inspires me.</p>
+
+           <a href="resume.pdf" target="top"><div class="button-default button-style-black">View Resume</div></a>
+        </div>
+
+  </div>
+  </div>
+  </section>
+
+
+<?php include "parts/footer.php"?>
+
+</body>
+</html>
+
+
+
+
