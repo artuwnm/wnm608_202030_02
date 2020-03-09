1689c191ee44500f8ad42716003ddd66b512b07f
Fifi Law <59303902+fifilaw@users.noreply.github.com>
2020-03-08 23:58:41 -0700

    updated styleguide

--- law.fifi/styleguide/index.js
+++ law.fifi/styleguide/index.js
@@ -1,3 +1,20 @@
+// Hamburger Menu
+
 $(".hamburger-menu").click(function(){
 		$(this).parent().toggleClass("activated");
 	});
+
+
+// Accordion created by Fifi Law
+
+$(".accordion").click(function(){
+	$(this).toggleClass("accordion-active");
+	
+	if($(this).hasClass("accordion-active")){
+		$(this).find(".accordion-icon").text("-");
+	}else{
+		$(this).find(".accordion-icon").text("+");
+	}
+
+	
+})
