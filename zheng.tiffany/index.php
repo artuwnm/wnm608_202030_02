a8d92c7d2b3bab92dc34f0a77d5ea97e9f2f18e0
Tiffany <tiffzheng1@gmail.com>
2020-04-01 16:24:58 -0700

    Completed navbar part

--- zheng.tiffany/index.php
+++ zheng.tiffany/index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-	<title>Landing Page</title>
+	<title>Stylista</title>
 	
 	<?php include "parts/meta.php" ?>
 	
--- zheng.tiffany/parts/navbar.php
+++ zheng.tiffany/parts/navbar.php
@@ -1,39 +1,57 @@
-<!-- <header class="navbar">
-	<div class="container display-flex">
-		<div class="flex-stretch">
-			<h1>Store</h1>
+<header class="store-header">
+	<div class="row container">
+		<div class="logo">
+			<a href="#">
+				<img src="images/stylista.png" alt="Stylista">
+			</a>
 		</div>
-		<nav class="nav flex-none">
-			<ul class="display-flex">
-				<li><a href="product_list.php">Shop</a></li>
-				<li><a href="#">Our Story</a></li>
-				<li><a href="#">FAQs</a></li>
-			</ul>
-		</nav>
-	</div>
-</header> -->
-
-<header class="navbar">
-	<nav class="container display-flex">
-		<a href="#" class="nav-bar__logo flex-none">
-			<h1>Stylista</h1>
-		</a>
-		<ul class="nav-bar__menu nav-bar__menu--main display-flex">
-			<li><a href="#">Shop</a></li>
-			<li><a href="#">Our Story</a></li>
-			<li><a href="#">FAQs</a></li>
+		<ul class="ctas display-flex">
+			<li class="search">
+				<form action="/search" method="get" id="searchbox">
+					<input type="text" name="q" class="search-input" id="q" placeholder="Search">
+					<input type="hidden" name="type" value="product">
+				</form>
+				<a href="#" class="search-icon">
+					<img src="images/search.svg" width="20px" height="20px" alt="Search">
+				</a>
+			</li>
+			<li>
+				<a href="/bag" class="bag-icon">
+					<img src="images/bag.svg" width="20px" height="20px" alt="Shopping Bag">
+				</a>
+			</li>
 		</ul>
-		<ul class="nav-bar__menu nav-bar__menu--ctas display-flex">
-			<li>
-				<a href="#">
-					<img src="images/search.svg" width="15px" height="15px" alt="Search">
-				</a>
-			</li>
-			<li>
-				<a href="#">
-					<img src="images/bag.svg" width="15px" height="15px" alt="Shopping Bag">
-				</a>
-			</li>
+	</div>
+	<nav class="container display-flex">
+		<ul>
+			<li class="dropdown">
+				<a href="#">Shop<span class="chevron bottom"></span></a>
+				<ul class="dropdown-content">
+					<li><a href="#">All</a></li>
+					<li><a href="#">Women's</a></li>
+					<li><a href="#">Men's</a></li>
+				</ul>
+		  	</li>
+		  	<li><a href="#">Our Story</a></li>
+		  	<li><a href="#">FAQs</a></li>
 		</ul>
 	</nav>
 </header>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
