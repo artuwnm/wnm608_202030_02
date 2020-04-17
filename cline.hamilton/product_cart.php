e3dc129d3f064e8c1f57f861d241f671fb62a193
ham <ham@ham.com>
2020-04-16 18:09:54 -0700

    module 10

--- cline.hamilton/product_cart.php
+++ cline.hamilton/product_cart.php
@@ -1,4 +1,19 @@
-<!DOCTYPE html>
+<?php
+
+include_once "lib/php/functions.php";
+include_once "parts/templates.php";
+
+$result = getRows(
+	makeConn(),
+	"SELECT *
+	FROM `products`
+	WHERE `id` IN (2,5,8)
+	"
+);
+
+// print_p($result);
+
+?><!DOCTYPE html>
 <html lang="en">
 <head>
 	<title>Store: Cart</title>
@@ -11,10 +26,38 @@
 	<?php include "parts/navbar.php" ?>
 
 	<div class="container">
-		<div class="card soft">
+		<nav class="nav-crumbs" style="margin:1em 0">
+			<ul>
+				<li><a href="product_list.php">Back</a></li>
+			</ul>
+		</nav>
 
-			<h2>Cart Page</h2>
-		
+		<div class="grid gap">
+			<div class="col-xs-12 col-md-8">
+				<div class="card">
+				<?php
+				echo array_reduce($result,'cartListTemplate');
+				?>
+				</div>
+			</div>
+			<div class="col-xs-12 col-md-4">
+				<div class="card">
+					<h2>Totals</h2>
+					<div>
+						<strong>Products</strong>
+						<span>$13</span>
+					</div>
+					<div>
+						<strong>Tax</strong>
+						<span>$13</span>
+					</div>
+					<div>
+						<strong>Total</strong>
+						<span>$13</span>
+					</div>
+					<div><a href="product_checkout.php" class="form-button">Checkout</a></div>
+				</div>
+			</div>
 		</div>
 	</div>
 	
