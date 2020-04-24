dc9b90eb1f1f39759526dcaaf8fb69235f61a5d6
ham <ham@ham.com>
2020-04-23 18:17:00 -0700

    module 11

--- cline.hamilton/product_item.php
+++ cline.hamilton/product_item.php
@@ -14,7 +14,7 @@ $o = $result[0];
 
 $thumbs = explode(",", $o->images);
 
-// print_p($result);
+// print_p($_SESSION);
 
 ?><!DOCTYPE html>
 <html lang="en">
@@ -53,16 +53,38 @@ $thumbs = explode(",", $o->images);
 				</div>
 			</div>
 			<div class="col-xs-12 col-md-5">
-				<div class="card soft">
-					<h2><?= $o->title ?></h2>
-					<div class="product-description">
-						<div class="product-price">&dollar;<?= $o->price ?></div>
+				<form class="card soft flat" method="get" action="data/form_actions.php">
+					<div class="card-section">
+						<h2><?= $o->title ?></h2>
+						<div class="product-description">
+							<div class="product-price">&dollar;<?= $o->price ?></div>
+						</div>
 					</div>
 
-					<div>
-						<a href="product_added_to_cart.php" class="form-button">Add To Cart</a>
+					<div class="card-section">
+						<label class="form-label">Amount</label>
+						<select name="amount" class="form-button">
+							<!-- option*10>{$} -->
+							<option>1</option>
+							<option>2</option>
+							<option>3</option>
+							<option>4</option>
+							<option>5</option>
+							<option>6</option>
+							<option>7</option>
+							<option>8</option>
+							<option>9</option>
+							<option>10</option>
+						</select>
 					</div>
-				</div>
+
+					<div class="card-section">
+						<input type="hidden" name="action" value="add-to-cart">
+						<input type="hidden" name="id" value="<?= $o->id ?>">
+						<input type="hidden" name="price" value="<?= $o->price ?>">
+						<input type="submit" class="form-button" value="Add To Cart">
+					</div>
+				</form>
 			</div>
 		</div>
 
