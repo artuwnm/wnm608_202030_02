4a55c1de552f6306ec863a70dc20bc980472a338
Cecelia0904 <31228903+Cecelia0904@users.noreply.github.com>
2020-04-29 22:18:40 -0700

    module 12

--- cao.cecelia/product_item.php
+++ cao.cecelia/product_item.php
@@ -72,34 +72,23 @@ $(".thumb-images img").on("click",function(){
 			</div>
 			
 			<div class="col-xs-12 col-md-5">
+				<form method="get" action="data/form_actions.php">
 			<div>
 				<h2><?= $o->name ?></h2>
 				<div class="product-description">
-				<h4><?= $o->description ?></h4>
+				<p><?= $o->description ?></p>
 			</div>
 			</div>
 		
 		<div id="accordion">
 			<ul class="a-container">
   <!-- item01 -->
-  <li class="a-items">
-    <input type="radio" name="ac" id="a1" class="hidden" />
-    <label for="a1">DESCRIPTION</label>
-    <div class="a-content">
-      
-    <div class="product-description">
-				<?= $o->description ?>
-			</div>
-    </div>
-  </li>
-  
     <!-- item02 -->
     <li class="a-items">
     <input type="radio" name="ac" id="a2" class="hidden"/>
-    <label for="a2">SHIPPING & RETURNING</label>
+    <label for="a2">Shipping & Returning</label>
     <div class="a-content">
-      
-     Lorem ipsum dolor sit amet, quidem ad necessitatibus fugiat consectetur veritatis laboriosam ut soluta quis deleniti deserunt! Voluptate qui excepturi architecto.
+      <p>Most J. Hannah fine jewelry is handmade to order. Due to the current closure of non-essential businesses in Los Angeles we cannot guarantee our regular lead time of 1-4 weeks. We are still accepting orders despite these delays and encourage customers to browse our index of in stock pieces which are able to ship in 1-2 days. </p>
     </div>
   </li>
   
@@ -108,26 +97,28 @@ $(".thumb-images img").on("click",function(){
     <input type="radio" name="ac" id="a3" class="hidden"/>
     <label for="a3">Keep In Touch</label>
     <div class="a-content">
-     
-     Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quam beatae maiores possimus sequi non quidem ad necessitatibus fugiat consectetur veritatis laboriosam ut soluta.
+     <p>Please email inquiries@jhannahjewelry.com for more information.</p>
     </div>
   </li>
 </ul>
 
 		</div>
 		<div class="display-flex flex-justify-center">
-			<a href="cart.php" class="linkbutton">
-				ADD TO CART <div class="product-price">&dollar;<?= $o->price ?></div>
+			
+						<input type="hidden" name="action" value="add-to-cart">
+						<input type="hidden" name="id" value="<?= $o->id ?>">
+						<input type="hidden" name="price" value="<?= $o->price ?>">
+						<input type="submit" class="form-button" value="Add To Cart &dollar;<?= $o->price ?>">
 
-			</a>
 			</div>
+				</form>
 	</div>
 </div>
 </div>
 
 <div class="container">
 <div class="grid gap">
-	<div class="col-xs-6 col-md-4">
+	<div class="col-xs-12 col-md-4">
 	<a href="product_item.php?id=$o->id" class="display-block">
 		<figure class="product-figure">
 			<div class="imagedecorarionrecommendation display-flex flex-align-center flex-justify-center">
@@ -139,14 +130,14 @@ $(".thumb-images img").on("click",function(){
 	</a>
 </div>
 
-	<div class="col-xs-6 col-md-4">
+	<div class="col-xs-12 col-md-4">
 	<a href="product_item.php?id=$o->id" class="display-block">
 		<figure class="product-figure">
 			<div class="product-image">
 				<img src="images/babyearings_thumbnail.jpg" alt="">
 			</div>
 			<div class="form-control display-flex">
-				<div class="flex-none">Baby Earings</div>
+				<div class="flex-none">Baby Earrings</div>
 				<div class="flex-stretch"></div>
 				<div class="flex-none">$98.98</div>
 			</div>
@@ -155,14 +146,14 @@ $(".thumb-images img").on("click",function(){
 	</a>
 </div>
 	
-	<div class="col-xs-6 col-md-4">
+	<div class="col-xs-12 col-md-4">
 	<a href="product_item.php?id=$o->id" class="display-block">
 		<figure class="product-figure">
 			<div class="product-image">
 				<img src="images/babyearings_thumbnail.jpg" alt="">
 			</div>
 			<div class="form-control display-flex">
-				<div class="flex-none">Baby Earings</div>
+				<div class="flex-none">Baby Earrings</div>
 				<div class="flex-stretch"></div>
 				<div class="flex-none">$98.98</div>
 			</div>
@@ -172,7 +163,6 @@ $(".thumb-images img").on("click",function(){
 </div>
 		</div>
 </div>
-	
 </body>
 <?php include "parts/footer.php" ?>
 </html>
