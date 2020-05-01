393b4e071ae7f4e9b5529d9a57f971dd5efa17fa
ham <ham@ham.com>
2020-04-30 18:26:36 -0700

    module 12

--- cline.hamilton/product_list.php
+++ cline.hamilton/product_list.php
@@ -1,18 +1,6 @@
 <?php
 
 include_once "lib/php/functions.php";
-include_once "parts/templates.php";
-
-$result = getRows(
-	makeConn(),
-	"SELECT *
-	FROM `products`
-	ORDER BY `date_create` DESC
-	LIMIT 12
-	"
-);
-
-// print_p($result);
 
 ?><!DOCTYPE html>
 <html lang="en">
@@ -20,6 +8,10 @@ $result = getRows(
 	<title>Store: Product List</title>
 
 	<?php include "parts/meta.php" ?>
+
+	<script src="lib/js/functions.js"></script>
+	<script src="js/templates.js"></script>
+	<script src="js/list.js"></script>
 
 </head>
 <body>
@@ -30,14 +22,35 @@ $result = getRows(
 	<div class="container">
 		<h2>Product List</h2>
 
-
-		<div class="grid gap">
-		<?php
-
-		echo array_reduce($result,'productListTemplate');
-
-		?>
+		<div class="form-control">
+			<form class="hotdog light" id="product-search">
+				<input type="search" class="search" placeholder="Search Products">
+			</form>
 		</div>
+		<div class="form-control display-flex">
+			<div class="flex-none">
+				<button type="button" class="form-button js-filter" data-column="category" data-value="">All</button>
+			</div>
+			<div class="flex-none">
+				<button type="button" class="form-button js-filter" data-column="category" data-value="fruit">Fruit</button>
+			</div>
+			<div class="flex-none">
+				<button type="button" class="form-button js-filter" data-column="category" data-value="vegetable">Vegetables</button>
+			</div>
+		</div>
+		<div class="form-control">
+			<!-- .form-select>select>option -->
+			<div class="form-select">
+				<select class="js-sort">
+					<option value="1">Newest</option>
+					<option value="2">Oldest</option>
+					<option value="3">Most Expensive</option>
+					<option value="4">Least Expensive</option>
+				</select>
+			</div>
+		</div>
+
+		<div class="grid gap productlist"></div>
 	</div>
 	
 </body>
