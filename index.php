93a2ed47dc65e709449149ae3b27872dd2b022d3
emilyh7777 <yinienpuma51@gmail.com>
2020-05-11 18:07:59 -0700

    Migrated HTML pages to PHP and separated header footer meta parts

---
+++ index.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+	<title>FenFong</title>
+	<?php include "parts/meta.php" ?>
+
+</head>
+
+<body>
+
+	<?php include "parts/header.php" ?>
+
+	<div class="hero-image">
+		<div class="hero-text">
+			<button class="site-button">SHOP NOW</button>
+		</div>
+	</div>
+	<div class="featured-items container">
+		<div class="grid gap">
+			<div class="col-xs-12 col-md-6 col-lg-3">
+				<div class="product-figure card light">
+					<img src="img/homepage_middle_01.png" alt="Soap 1">
+				</div>
+			</div>
+			<div class="col-xs-12 col-md-6 col-lg-3">
+				<div class="product-figure card light">
+					<img src="img/homepage_middle_02.png" alt="Soap 2">
+				</div>
+			</div>
+			<div class="col-xs-12 col-md-6 col-lg-3">
+				<div class="product-figure card light">
+					<img src="img/homepage_middle_03.png" alt="Soap 3">
+				</div>
+			</div>
+			<div class="col-xs-12 col-md-6 col-lg-3">
+				<div class="product-figure card light">
+					<img src="img/homepage_middle_04.png" alt="Soap 3">
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<?php include "parts/footer.php" ?>
+
+</body>
+
+</html>
---
+++ parts/meta.php
@@ -0,0 +1,7 @@
+<meta charset="UTF-8">
+<meta name="viewport"
+		content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+	<link href="https://fonts.googleapis.com/css2?family=Josefin+Sans&display=swap" rel="stylesheet">
+	<link rel="stylesheet" href="lib/css/styleguide.css">
+	<link rel="stylesheet" href="lib/css/gridsystem.css">
+	<link rel="stylesheet" href="css/storetheme.css">
