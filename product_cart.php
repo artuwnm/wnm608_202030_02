48223bcdcbe0a73a4945d5295396b1a6644efbda
emilyh7777 <yinienpuma51@gmail.com>
2020-05-11 18:21:06 -0700

    Create product_cart.php

---
+++ product_cart.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>Store: Product Cart</title>
+
+	<?php include "parts/meta.php" ?>
+
+</head>
+<body>
+
+	<?php include "parts/header.php" ?>
+
+
+	<div class="container">
+		<div class="card soft">
+
+			<h2>Cart Page</h2>
+
+		</div>
+	</div>
+
+	<?php include "parts/footer.php" ?>
+
+</body>
+</html> 
