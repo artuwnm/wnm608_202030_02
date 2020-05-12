f09c770673e2dc70937456bee2e1a8199cfcd235
emilyh7777 <yinienpuma51@gmail.com>
2020-05-11 18:20:12 -0700

    Fixed about us page

--- aboutus.php
+++ aboutus.php
@@ -1,57 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
 
-	<div class="featured-items container">
-		<div class="grid gap">
-			<div class="col-xs-12 col-md-6 col-lg-4">
-				<div class="product-figure card light">
-					<img src="img/soap.jpg" alt="Soap 1">
-				</div>
-			</div>
-			<div class="col-xs-12 col-md-6 col-lg-4">
-				<div class="product-figure card light">
+<head>
 
-					<img src="img/soap.jpg" alt="Soap 2">
-				</div>
-			</div>
-			<div class="col-xs-12 col-md-6 col-lg-4">
-				<div class="product-figure card light">
+	<title>FenFong: About Us</title>
+	<?php include "parts/meta.php" ?>
 
-					<img src="img/soap.jpg" alt="Soap 3">
-				</div>
-			</div>
+</head>
+
+<body>
+
+	<?php include "parts/header.php" ?>
+
+	<div class="container">
+		<div class="card soft">
+
+			<h2>About Us</h2>
+
 		</div>
 	</div>
-	<footer class="footer">
-		<div class="container display-flex space-between">
-			<ul class="footer__menu--shop">
-				<li>
-					<h3>Shop</h3>
-				</li>
-				<li><a href="shop_all.php">All Collections</a></li>
-				<li><a href="shop_women.php">Hand Soap</a></li>
-				<li><a href="shop_men.php">Body Wash</a></li>
-			</ul>
-			<ul class="footer__menu--explore">
-				<li>
-					<h3>Explore</h3>
-				</li>
-				<li><a href="about.php">Our Story</a></li>
-				<li><a href="faqs.php">Frequently Asked Questions</a></li>
-				<li><a href="#">Contact</a></li>
-			</ul>
-			<ul class="footer__menu--form">
-				<li>
-					<h3>Stay Connected</h3>
-				</li>
-				<li>
-					<p>Join our newsletter to hear about new items, offers and more!</p>
-				</li>
-				<li>
-					<input type="text" class="form-input" placeholder="Sign up for email alerts">
-					<input type="submit" class="form-button" value="Join">
-				</li>
-			</ul>
-		</div>
-		<p class="footer-copyright center">© Copyright 2020, Fenfong</p>
-	</footer>
+
+	<?php include "parts/footer.php" ?>
+
 </body>
+
 </html>
